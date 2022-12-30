ceb3263fcc45f31d104997fc6bf5b32c3684e998
Carlos Quijada <carmanu09@gmail.com.com>
2022-12-30 13:46:19 -0600

    new endpoint added

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants{
     public static API_BASE_URL= 'https://crudcrud.com/api' ;
-    public static API_ID = '74f36f68182145a19f3c84c31b41915c';
+    public static API_ID = '4948919105ff4fb99ced69389e273950';
 }
